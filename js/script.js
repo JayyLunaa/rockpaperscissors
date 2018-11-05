6cd2190bd43f74a230a33f52b1bfdeb4e38fe350
Jair Luna <jm44179@gmail.com>
2018-11-05 23:04:59 +0000

    RPS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
+$("button").click(function(){
+$("#userChoice").text($("#input").val());
+});
 //GLOBAL VARIABLES
 /* global $ */
 
