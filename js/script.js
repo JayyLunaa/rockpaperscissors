ec9a0fc3ef4211b44199886334d0fff987821403
Jair Luna <jm44179@gmail.com>
2018-11-19 23:07:25 +0000

    week 2 RPS project

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 $("button").click(function(){
-$("#userChoice").text($("#input").val());
+    $("#userChoice").text($("#input").val());
+    var randomNumber=Math.random();
+    var computerChoice;
+    if (randomNumer > .666666){
+        $("#computerChoice").html("rock");
+    }
 });
 //GLOBAL VARIABLES
 /* global $ */
 
+var randomNumber=Math.random();
 
 
 // DOCUMENT READY FUNCTION BELOW
